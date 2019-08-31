7c1d7d4acf8aae0e2aa9aae2105e4cbddea970ce
Amir <eslami.amir76@gmail.com>
2019-08-31 15:37:08 +0430

    feat(header): improve header using styled-components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import Navbar from 'reactstrap/lib/Navbar';
 import NavbarBrand from 'reactstrap/lib/NavbarBrand';
@@ -7,6 +7,7 @@ import Nav from 'reactstrap/lib/Nav';
 import NavItem from 'reactstrap/lib/NavItem';
 import Collapse from 'reactstrap/lib/Collapse';
 import { Link } from 'react-router-dom';
+import StyledHeader from './StyledHeader';
 
 const Header = (_props) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -16,7 +17,7 @@ const Header = (_props) => {
     };
 
     return (
-        <header>
+        <StyledHeader>
             <Navbar expand='md'>
                 <NavbarBrand href='/'>Scoutbase</NavbarBrand>
                 <NavbarToggler onClick={toggleNavbar} />
@@ -31,7 +32,7 @@ const Header = (_props) => {
                     </Nav>
                 </Collapse>
             </Navbar>
-        </header>
+        </StyledHeader>
     )
 };
 
---
+++ src/components/Header/StyledHeader.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    background-color: #eeeeee;
+
+    .navbar-nav {
+        .nav-item {
+            margin: 0 .5em;
+        }
+    }
+`;
+
+export default StyledHeader;
