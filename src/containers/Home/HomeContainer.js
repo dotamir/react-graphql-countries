98bde7f0e5b29af4774f62d3819394ea2a3c241e
Amir <eslami.amir76@gmail.com>
2019-08-31 19:01:50 +0430

    fix(home): add description texts

--- src/containers/Home/HomeContainer.js
+++ src/containers/Home/HomeContainer.js
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
     return (
         <div className="home">
-            Home page
+            <h1>Home page</h1>
+            <p>This application is based on Create-React-App, but I've made some changes on structure to make it organized.</p>
+            <p>You might see the "componentWillMount" warning in console, which is related to "react-loadable" package and they didn't migrate to the latest react version yet.</p>
         </div>
     );
 };
